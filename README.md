375345e41006c35638f89113c919aae446f6e076
Marc Lucraft <marclucraft@gmail.com>
2023-11-09 19:18:08 +0000

    Created Readme

---
+++ README.md
@@ -0,0 +1,4 @@
+<picture>
+  <source media="(prefers-color-scheme: dark)" srcset="./dark.png">
+  <img alt="UnoSignal Logo'" src="./light.png">
+</picture>
--- onesignal.php
+++ onesignal.php
@@ -3,9 +3,9 @@
 defined('ABSPATH') or die('This page may not be accessed directly.');
 
 /*
- * Plugin Name: UnoSignal (Marc's Version)
+ * Plugin Name: UnoSignal
  * Plugin URI: https://github.com/marclucraft/unosignal
- * Description: Free web push notifications, by OneSignal.
+ * Description: Free web push notifications. Unofficial OneSignal/WordPress integration.
  * Version: 0.1
  * Author: Marc Lucraft
  * Author URI: https://github.com/marclucraft
